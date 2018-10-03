d9633f16596431d014d3f71e3147e0cd5b27aefc
Florian Fusseder <fussederflorian@gmail.com>
2018-10-03 14:04:56 +0200

    Setup cloud with web request service

---
+++ spring-web-request/src/main/java/springbootdemo/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package springbootdemo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
